826e066666a4cdf13cdfa9dff63d0775546b4d79
horse <horsescary@gmail.com>
2022-11-19 16:40:02 -0500

    things

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let myCheese = false
 
+/*
 register("chat", () => {
     if (!myCheese) {
         ChatLib.say('/l')
     }
 }).setChatCriteria("&r&9Party &8>${*}&f: &r[Diana Utils] Cheese!&r")
+*/
 
 register("chat", () => {
     myCheese = true
@@ -16,11 +18,21 @@ register("chat", () => {
     myCheese = false
 }).setChatCriteria("&r&e&lCHEESE!&r&7 You buffed &r${*}&r&7 giving them &r&b+${*}✯ Magic Find&r&7 for &r&a${*}&r&7 seconds!&r")
 
+register("tick", () => {
+    World.getAllEntities().forEach(entity => {
+        if (entity.getname.toLowerCase().includes("inquis")) {
+            ChatLib.say("/pc inquis!")
+        }
+    }
+
+    )
+})
+
 register("chat", () => {
     ChatLib.say('/play sb')
 }).setChatCriteria("&r&9Party &8>${*}&f: &r[Diana Utils] Cheese obtained!&r")
 
-    ```
+/*
 &r&9Party &8> &b[MVP&0+&b] jperrm&f: &rIt was runic, I swear!&r
 
 &r&9Party &8> &b[MVP&0+&b] jperrm&f: &rUwU&r
@@ -28,5 +40,4 @@ register("chat", () => {
 
 CHEESE! You buffed Brendidy giving them +7✯ Magic Find for 
 &r&e&lCHEESE!&r&7 You buffed &r&bBrendidy&r&7 giving them &r&b+7✯ Magic Find&r&7 for &r&a60&r&7 seconds!&r
-
-```
+*/
