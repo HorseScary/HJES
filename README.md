70be266d13f6cb4f9473b00de40b274fc58e63e2
horse <horsescary@gmail.com>
2022-11-20 11:30:19 -0500

    runic notification

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Diana Utils
 
+Makes rat pet stuff more easier for diana
+
+## Features
+
+- 
+
 To install, download the repo and put the dianaUtils folder into your ct modules folder. 
--- index.js
+++ index.js
@@ -13,27 +13,39 @@ register("chat", () => {
 }).setChatCriteria("&r&e&lCHEESE! &r&7You smell Cheese nearby!&r")
 
 register("chat", () => {
-    ChatLib.say('[Diana Utils] Cheese obtained!')
-    myCheese = false
+    ChatLib.say('/pc [Diana Utils] Cheese obtained!')
 }).setChatCriteria("&r&e&lCHEESE!&r&7 You buffed &r${*}&r&7 giving them &r&b+${*}✯ Magic Find&r&7 for &r&a${*}&r&7 seconds!&r")
 
 register("chat", () => {
     World.getAllEntities().forEach(entity => {
         if (entity.getName().toLowerCase().includes("inquis") && !inquisExists) {
             inquisExists = true
-            ChatLib.say("/pc inquis!")
+            ChatLib.say(`/pc [Diana Utils] Inquis at ${entity.getLastX()} ${entity.getLastY()} ${entity.getLastZ()}}`)
+        }
+        if (entity.getName().toLowerCase().includes("champ") && !inquisExists) {
+            setTimeout(() => {
+                ChatLib.say(`/pc [Diana Utils] Inquis `)
+            }, 250)
+            setTimeout(() => {
+                ChatLib.say(`x: ${parseInt(entity.getLastX())}, y: ${parseInt(entity.getLastY())}, z: ${parseInt(entity.getLastZ())}`)
+            }, 250)
         }
     })
 }).setChatCriteria("${*}&r&eYou dug out &r&2a Minos Champion&r&e!&r")
 
 register("tick", () => {
     World.getAllEntities().forEach(entity => {
-        if (entity.getName().toLowerCase().includes("inquis") && (entity.isDead()) && inquisExists) {
+        if (entity.getName().toLowerCase().includes("inquis") && (entity.isDead())) {
             ChatLib.say("/pc inquis dead!")
             inquisExists = false
+            myCheese = false
         }
     })
 })
+
+register("chat", () => {
+    ChatLib.say("/pc It was runic i swear!")
+}).setChatCriteria("&r&c ☠ ${*} was killed by &r&2Exalted Gaia Construct&r&7&r&7.&r")
 
 register("chat", () => {
     ChatLib.say('/play sb')
@@ -48,4 +60,7 @@ register("chat", () => {
 CHEESE! You buffed Brendidy giving them +7✯ Magic Find for 
 &r&e&lCHEESE!&r&7 You buffed &r&bBrendidy&r&7 giving them &r&b+7✯ Magic Find&r&7 for &r&a60&r&7 seconds!&r
 &r&c&lYikes! &r&eYou dug out &r&2a Minos Champion&r&e!&r
+
+&r&c ☠ &r&7You were killed by &r&2Exalted Gaia Construct&r&7&r&7.&r
+&r&c ☠ &r&7&r&bjperrm&r&7 was killed by &r&2Exalted Gaia Construct&r&7&r&7.&r
 */
