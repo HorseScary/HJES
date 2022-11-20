d01bf8a18d2c4583ffa198d83e61668713fe6fc6
Vecolity <42125737+Vecolity@users.noreply.github.com>
2022-11-20 14:19:09 -0600

    Update index.js
    working diana utils (jperm styke)

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 let myCheese = false
 let inquisExists = false
-    
+
+function sendcoords() {
+    let playerX = Player.getX();
+    let playerY = Player.getY();
+    let playerZ = Player.getZ();
+    setTimeout(() => {
+        ChatLib.say(`/pc x: ${playerX}, y: ${playerY}, z: ${playerZ}`)
+    }, 500)
+}
 
 register("chat", () => {
     if (!myCheese && !inquisExists) {
@@ -14,7 +22,7 @@ register("chat", () => {
 }).setChatCriteria("&r&e&lCHEESE! &r&7You smell Cheese nearby!&r")
 
 register("chat", () => {
-    ChatLib.say('[Diana Utils] Cheese obtained!')
+    ChatLib.say('/pc [Diana Utils] Cheese obtained!')
     myCheese = false
 }).setChatCriteria("&r&e&lCHEESE!&r&7 You buffed &r${*}&r&7 giving them &r&b+${*}✯ Magic Find&r&7 for &r&a${*}&r&7 seconds!&r")
 
@@ -29,12 +37,11 @@ register("chat", () => {
 }).setChatCriteria("${*}&r&eYou dug out &r&2a Minos Champion&r&e!&r")
 
 register("tick", () => {
-    World.getAllEntities().forEach(entity => {
-        if (entity.getName().toLowerCase().includes("inquis") && (entity.isDead()) && inquisExists) {
-            ChatLib.say("/pc inquis dead!")
+    if(inquisExists) {
+        setTimeout(() => {
             inquisExists = false
-        }
-    })
+        }, 100000)
+    }
 })
 
 register("chat", () => {
@@ -45,7 +52,7 @@ register("chat", () => {
 register("command", () => {
     inquisExists = true
     ChatLib.say("/pc inquis! (manual input)")
-    ChatLib.say(`/pc x: ${playerX}, y: ${playerY}, z: ${playerZ}`)
+    sendcoords()
 }).setName('myinq')
 
 register("command", () => {
@@ -54,7 +61,7 @@ register("command", () => {
 }).setName('deadinq')
 
 register("command", () => {
-    ChatLib.say(`/pc x: ${playerX}, y: ${playerY}, z: ${playerZ}`)
+    sendcoords()
 }).setName('sc')
 
 register("chat", () => {
@@ -73,19 +80,4 @@ register("chat", () => {
     ChatLib.say("/gc [SBE] RARE DROP! Dwarf Turtle Shelmet")
 }).setChatCriteria("&3[SBE] &a&6&lRARE DROP! &9Dwarf Turtle Shelmet&r")
 
-
-
-/*
-&r&9Party &8> &b[MVP&0+&b] jperrm&f: &rIt was runic, I swear!&r
-
-&r&9Party &8> &b[MVP&0+&b] jperrm&f: &rUwU&r
-&r&e&lCHEESE! &r&7You smell Cheese nearby!&r
-
-&r&c ☠ &r&7You were killed by &r&2Exalted Gaia Construct&r&7&r&7.&r
-
-&3[SBE] &a&6&lRARE DROP! &5Antique Remedies&r
-
-CHEESE! You buffed Brendidy giving them +7✯ Magic Find for 
-&r&e&lCHEESE!&r&7 You buffed &r&bBrendidy&r&7 giving them &r&b+7✯ Magic Find&r&7 for &r&a60&r&7 seconds!&r
-&r&c&lYikes! &r&eYou dug out &r&2a Minos Champion&r&e!&r
-*/
+//comment out the drop posters if you dont want them i just watned to annoy my guild
