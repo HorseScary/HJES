073b092ca5d32ed4ba3b1b9287ffbe60e861776b
Vi <panagen3002@gmail.com>
2022-11-21 10:53:13 -0500

    added help message to /hjes

--- HJES/index.js
+++ HJES/index.js
@@ -1,9 +1,28 @@
 import Settings from "./config";
 import "./features/diana"
 import "./features/trollege"
+import { helpHelper, HJESMessage } from "./functions";
 
-register("command", () => {
-    Settings.openGUI()
+
+register("command", (arg) => {
+    helpMessage = helpHelper({
+        '__title__': '',
+        '__subtitle__': 'Diana',
+        'inquis': 'Diana inquisitor functions',
+        '__subtitle__': 'Trollege',
+        'sendPenis': 'sends coordinates of a penis. works best with soopy player coordinates thing'
+    })
+    if (arg == "help") {
+        ChatLib.chat(helpMessage)
+    }
+
+    else if (!arg) {
+        Settings.openGUI()
+    }
+
+    else {
+        ChatLib.chat(HJESMessage(`${arg} is not a valid option. Type /HJES help for help.`))
+    }
 }).setName("HJES", true);
 
 // Test command for settings things
