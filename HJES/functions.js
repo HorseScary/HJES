78576ed7790e2b72484bc4f2835531a9654084e9
Vi <panagen3002@gmail.com>
2022-11-21 09:58:12 -0500

    functions.js created work on helphelper function

---
+++ HJES/functions.js
@@ -0,0 +1,33 @@
+/*
+Generates help messages from an object
+use __title__ for the main title of the help message and __subtitle__ for subtitles (wow no way crazy)
+*/
+export function helpHelper(commandInfo) {
+    helpMessage = ``
+    lastItem = (function () {
+        entries = Object.entries(commandInfo)
+        return (entries[entries.length][0])
+    })()
+
+    for (i in commandInfo) {
+        if (i == "__title__") {
+            helpMessage += `&l&d[&aH&3J&dES&f ${commandInfo[i]}&d]`
+        }
+        else if (i == "__subtitle__") {
+            helpMessage += `&l&d[&f${commandInfo[i]}&d]`
+        }
+        else if (i == "__warning__") {
+            helpMessage += `&d[&aH&3J&dES]&f ${commandInfo}`
+        }
+        else if (i == "__custom__") {
+            helpMessage += `${commandInfo[i]}`
+        }
+        else {
+            helpMessage += `&b${i}:&f ${commandInfo[i]}`
+        }
+
+        if (i != lastItem) {
+            helpMessage += `\n`
+        }
+    }
+}
