a85cbbea2e3a1fbedf2ef0a7e584eed2e1c96205
Vi <panagen3002@gmail.com>
2022-11-25 00:26:23 -0500

    added footer

--- website-resources/main.js
+++ website-resources/main.js
@@ -1,3 +1,21 @@
+window.onload = function () { footer() }
+
+function footer() {
+    foot = document.getElementsByTagName("footer").item(0)
+
+    for (let i = 0; i < foot.children.length; i++) {
+        let child = foot.children.item(i)
+        if (child.tagName == "A") {
+            if (child.innerHTML.includes("HTML")) {
+                child.setAttribute("href", `http://validator.w3.org/check?uri=${window.location.href}`)
+            }
+            else if (child.innerHTML.includes("CSS")) {
+                child.setAttribute("href", `http://jigsaw.w3.org/css-validator/validator?uri=${window.location.href}?profile=css3`)
+            }
+        }
+    }
+}
+
 function dropdown(element) {
     dropdownElement = document.getElementById(element)
     dropdownStatus = dropdownElement.getAttribute("dropdownStatus")
