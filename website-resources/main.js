c95c938e4ba9a4242774ce14fba4cf6992d138a3
Vi <panagen3002@gmail.com>
2022-11-24 23:48:22 -0500

    work on website

---
+++ website-resources/main.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+
+}
+
+function dropdown(element) {
+    dropdownElement = document.getElementById(element)
+    dropdownStatus = dropdownElement.getAttribute("dropdownStatus")
+
+    dropdownChildren = dropdownElement.childNodes
+    dropdownDiv = undefined
+    dropdownTitle = undefined
+
+    for (i = 0; i < dropdownChildren.length; i++) {
+        if (dropdownChildren[i].nodeName == "DIV" && !dropdownDiv) {
+            dropdownDiv = dropdownChildren[i]
+        }
+        else if (dropdownChildren[i].nodeName == "H2" && !dropdownTitle) {
+            dropdownTitleArrow = dropdownChildren[i].firstChild
+        }
+    }
+
+    if (dropdownStatus == "up") {
+        dropdownDiv.setAttribute("style", "display: block;")
+        dropdownElement.setAttribute("dropdownStatus", "down")
+        dropdownTitleArrow.setAttribute("class", "")
+
+    }
+    else if (dropdownStatus == "down") {
+        dropdownDiv.setAttribute("style", "display: none;")
+        dropdownElement.setAttribute("dropdownStatus", "up")
+        dropdownTitleArrow.setAttribute("class", "dropdownArrowDown")
+    }
+}
