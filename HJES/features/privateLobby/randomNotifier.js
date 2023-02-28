07f74488c2eef39d6f3846f9e9777756081a85b8
Vi <panagen3002@gmail.com>
2023-02-28 13:40:26 -0500

    announce number of randoms in new lobby when swapping

--- HJES/features/privateLobby/randomNotifier.js
+++ HJES/features/privateLobby/randomNotifier.js
@@ -2,7 +2,7 @@ import Settings from "../../config"
 import { HJESMessage, helpHelper } from "../../functions";
 
 let randomDetected = false
-let lobbyChecked = true
+let lobbyChecked = false
 let lobbyPlayers = 0
 let partyPlayers = 1
 
@@ -11,27 +11,48 @@ register("tick", () => {
     if (players) {
         players = parseInt(players.split("(")[1])
 
+        // private finder
         if (Settings.privateFinder && !lobbyChecked) {
             if (players == 1) {
-                lobbyChecked = true
                 randomDetected = false
                 ChatLib.chat(HJESMessage("Private lobby!"))
                 World.playSound("random.orb", 1, 1)
             }
         }
 
+        // random notifier
         if (Settings.randomNotifier) {
             if (players > partyPlayers) {
-                if (!randomDetected) {
-                    ChatLib.chat(HJESMessage("Theres a random in the lobby!"))
-                    World.playSound("random.orb", 1, 1)
+                randoms = players - partyPlayers
+                if (!lobbyChecked) {
+                    if (randoms != 1) {
+                        ChatLib.chat(HJESMessage(`There is ${randoms} random in this lobby.`))
+                    }
+                    else {
+                        ChatLib.chat(HJESMessage(`There are ${randoms} randoms in this lobby.`))
+                    }
                     randomDetected = true
                 }
-                else if (players > lobbyPlayers) {
-                    ChatLib.chat(HJESMessage(`Another random has joined! There are now ${players - partyPlayers} randoms.`))
-                }
-                else if (players < lobbyPlayers) {
-                    ChatLib.chat(HJESMessage(`A random has left! There are now ${players - partyPlayers} randoms.`))
+                else {
+                    if (!randomDetected) {
+                        ChatLib.chat(HJESMessage("Theres a random in the lobby!"))
+                        World.playSound("random.orb", 1, 1)
+                        randomDetected = true
+                    }
+                    // compares current player count to player count on last tick
+                    else if (players > lobbyPlayers) {
+                        ChatLib.chat(HJESMessage(`Another random has joined! There are now ${players - partyPlayers} randoms.`))
+                    }
+                    else if (players < lobbyPlayers) {
+                        if (randoms == 1) {
+
+                            ChatLib.chat(HJESMessage(`A random has left! There is now ${randoms} random.`))
+                        }
+                        else {
+                            ChatLib.chat(HJESMessage(`A random has left! There are now ${randoms} randoms.`))
+                        }
+                    }
+                    // if player count <= partyPlayers and a random has been detected...
                 }
             }
             else if (randomDetected) {
@@ -41,19 +62,12 @@ register("tick", () => {
 
             lobbyPlayers = players
         }
-
+        lobbyChecked = true
     }
-
 })
 
 register("worldLoad", () => {
-    if (Settings.randomNotifier && !partyPlayers) {
-        ChatLib.chat(HJESMessage("Run /pl or /partyplayers for random notifier to work!"))
-    }
-
-    if (Settings.privateFinder) {
-        lobbyChecked = false
-    }
+    lobbyChecked = false
 })
 
 register("command", (args) => {
@@ -104,9 +118,9 @@ register("chat", () => {
 }).setCriteria("${*}has left the party.&r")
 
 register("chat", () => {
-    partyPlayers = 0
+    partyPlayers = 1
 }).setCriteria("${*}has disbanded the party!&r")
 
 register("chat", () => {
-    partyPlayers = 0
+    partyPlayers = 1
 }).setCriteria("&cThe party was disbanded because all invites expired and the party was empty.&r")
