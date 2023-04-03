c800f3d38926545c9de84300449618f78fc5c619
Vi <panagen3002@gmail.com>
2023-04-03 16:22:45 -0400

    ideas for how to do things

--- HJES/handlers/chatHandler.js
+++ HJES/handlers/chatHandler.js
@@ -2,6 +2,8 @@ import { getChatQueue } from "./say"
 
 let messageCooldown = 0
 
+// TODO: register when player (or other another mod) sends a message and reset messageCooldown
+
 register("tick", () => {
     chatQueue = getChatQueue()
     if (chatQueue.length != 0 && messageCooldown == 0) {
@@ -16,3 +18,13 @@ register("tick", () => {
         messageCooldown++
     }
 })
+
+/*
+HANDLING ANIMATIONS
+- throw x number of nulls into the queue, ignore nulls
+    - if HJES needs to send a message (like for inquis) it will have to wait x number of ticks
+- special designator to put thing n ticks after another thing
+- special queue
+    - has time to wait for things in special queue
+    - thing gets added after the time is up
+*/
