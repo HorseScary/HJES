eb7f2c14c49dafd0b231159d116c5dde1bd7ba8e
Vi <panagen3002@gmail.com>
2023-04-12 13:20:54 -0400

    added hud handler code

--- HJES/handlers/hudHandler.js
+++ HJES/handlers/hudHandler.js
@@ -14,7 +14,41 @@ export function addToHUD(text, id) {
     hudPositions.save()
 }
 
+register("dragged", (dx, dy, x, y) => {
+    if (!testGUI.isOpen()) return;
+
+    if (selectedItem) {
+        hudPositions[selectedItem][0] = x
+        hudPositions[selectedItem][1] = y
+        hudPositions.save();
+    }
+})
+
+register("clicked", (x, y, button) => {
+    itemSelected = false;
+    keys = Object.keys(hudItems)
+    for (i = 0; i < keys.length; i++) {
+        itemX = hudPositions[keys[i]][0]
+        itemY = hudPositions[keys[i]][1]
+        textWidth = Renderer.getStringWidth(hudItems[keys[i]])
+        textHeight = 9
+        if (x >= itemX && x <= itemX + textWidth && y >= itemY && Y <= itemY + textHeight) {
+            selectedItem = keys[i]
+            itemSelected = true;
+        }
+    }
+
+    if (!itemSelected) {
+        selectedItem = null;
+    }
+})
+
 register("renderOverlay", () => {
+    if (hudGUI.isOpen()) {
+        middle = Renderer.screen.getWidth() / 2
+        Renderer.DrawStringWithShadow("[Move the things]", middle, 4)
+    }
+
     keys = Object.keys(hudItems)
     for (i = 0; i < keys.length; i++) {
         Renderer.drawStringWithShadow(hudItems[keys[i]], hudPositions[keys[i]][0], hudPositions[keys[i]][1])
@@ -22,5 +56,5 @@ register("renderOverlay", () => {
 })
 
 register("command", () => {
-
+    hudGUI.open()
 }).setName("hjesEditHUD")
